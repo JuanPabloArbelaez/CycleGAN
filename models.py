92a013859b4adb2f7b195caf13559ed3749386b0
Juan Pablo Arbelaez Gomez <juanpablo-arbelaez@hotmail.com>
2020-12-08 18:20:38 +0700

    Update models.py

--- models.py
+++ models.py
@@ -44,7 +44,7 @@ class ContractingBlock(nn.Module):
     """
     def __init__(self, input_channels, use_bn=True, kernel_size=3, activation="relu"):
         super().__init__()
-        self.conv1 = nn.Conv2d(input_channels, input_channels * 2, kernel_size=kernel_size, paddong=1, stride=2, padding_mode="reflect")
+        self.conv1 = nn.Conv2d(input_channels, input_channels * 2, kernel_size=kernel_size, padding=1, stride=2, padding_mode="reflect")
         self.activation = nn.ReLU() if activation == "relu" else nn.LeakyReLU(0.2)
         if use_bn:
             self.instancenorm = nn.InstanceNorm2d(input_channels * 2)
