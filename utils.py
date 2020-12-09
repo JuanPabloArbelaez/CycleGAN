777535b83bf9fb8778b6c0b6ae9b158ff8f0851a
Juan Pablo Arbelaez Gomez <juanpablo-arbelaez@hotmail.com>
2020-12-09 13:10:54 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 
 
 
-def show_tensor_images(image_tensor, num_images, size=(1, 28, 28)):
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
     image_tensor = (image_tensor + 1) / 2
     image_shifted = image_tensor
     image_unflat = image_shifted.detach().cpu().view(-1, *size)
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
-    plt.imshow(image_grid.permut(1, 2, 0).squeeze())
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
     plt.show()
 
 
